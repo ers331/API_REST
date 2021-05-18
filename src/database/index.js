61743e77dc6ee7aecc42e776a26ee7633c834f3c
Everson Ramiro da Silva <80833918+ers331@users.noreply.github.com>
2021-05-18 11:00:31 -0300

    Delete src directory

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
-import Cliente from '../models/Cliente';
-
-const models = [Cliente];
-
-const connection = new Sequelize(databaseConfig);
-models.forEach((model) => model.init(connection));
