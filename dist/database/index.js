db5eaf00de8f364aab3326b6f405a2d0953b8f5a
Everson Ramiro da Silva <80833918+ers331@users.noreply.github.com>
2021-05-18 11:00:23 -0300

    Delete dist directory

--- dist/database/index.js
+++
@@ -1,8 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize'); var _sequelize2 = _interopRequireDefault(_sequelize);
-var _database = require('../config/database'); var _database2 = _interopRequireDefault(_database);
-var _Cliente = require('../models/Cliente'); var _Cliente2 = _interopRequireDefault(_Cliente);
-
-const models = [_Cliente2.default];
-
-const connection = new (0, _sequelize2.default)(_database2.default);
-models.forEach((model) => model.init(connection));
